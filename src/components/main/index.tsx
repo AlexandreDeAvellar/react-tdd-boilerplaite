47594e009f7f711faadaeb8b5fcd93befa1a97d9
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2021-09-08 20:31:34 -0300

    test: create new Main snapshot and ensure render the colors correctly

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -4,7 +4,7 @@ const Main = () => (
   <S.Wrapper>
     <S.Logo
       src="/img/logo.svg"
-      alt="Imagem de um átomo e React Avançado escrito ao lado."
+      alt="Imagem de um átomo e React Avançado escrito ao lado. "
     />
     <S.Title>React Avançado</S.Title>
     <S.Description>
