437f1285b3c2e928e921e541e8e870ff49b39466
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2021-09-21 19:12:17 -0300

    feat: add link manifest on component App

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,8 @@ function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>React Avançado - Boilerplate</title>
-        <link rel="shortcut icon" href="" />
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
           content="A project starter to work with Typescript, React, NextJS and Styled Components"
